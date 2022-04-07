e4e8335a04d226be84597889c499dd6c2e78639f
Bernhard-Franzl <75139279+Bernhard-Franzl@users.noreply.github.com>
2022-04-07 15:31:39 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+import utility_functions.utility_functions as utils
+from sklearn.decomposition import PCA
+
+# data loading and preprocessing
+# load wien museum data 
+wm_original = pd.read_csv("data/wien_museum.csv") # note  file location
+# take only interesting columns
+wm_filtered = wm_original[wm_original.columns[[0,3,4,5,6,7,8]]]
+# merge text data of all columns into one 
+wm_filtered = wm_filtered.assign(full_text = wm_filtered[wm_filtered.columns[1:]].apply(lambda x: ' '.join(x.dropna().astype(str)),axis=1))
+# apply preprocessing pipeline
+wm_preprocessed = utils.preprocessing(wm_filtered, "full_text")
+print("Preprocessing: Done")
+
+# load sentence embedding
+sentence_embeddings = np.loadtxt('sentence_embeddings.csv', delimiter=',')
+print("Loading: Done")
+
+# create subset of sentence embeddings (62591 = all samples)
+nr_samples = 62591 
+sub_sample = sentence_embeddings[:nr_samples]
+
+# perform PCA: down project to 3 dimensional vector
+pca = PCA(n_components=3)
+embeddings_pca = pca.fit_transform(sub_sample)
+
+# plot results 
+sent_vec_generation_method = "Sentence Transformer" # only for plot title 
+dim_reduction_method = "PCA" # only for plot title
+color = "classifications" # column used for coloring
+utils.plot(meta_data = wm_filtered, embeddings = embeddings_pca, 
+           nr_samples = nr_samples,  color = color,
+           sent_vec_gen_method = sent_vec_generation_method, 
+           dim_red_method = dim_reduction_method)
---
+++ utility_functions/utility_functions.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import numpy as np
+import nltk
+#nltk.download('stopwords')
+from nltk.corpus import stopwords
+import plotly.express as px
+
+def preprocessing(text_data: pd.DataFrame, column_name: str) -> pd.DataFrame:
+
+    helper = text_data.copy(deep = True)
+    helper = helper.assign(pre_text = helper[column_name]) 
+    tokenizer = nltk.RegexpTokenizer(r"\w+")
+    cachedStopWords = stopwords.words('german')
+    
+    # noch kein Stemmer -> könnt ma auch probieren hab online ein bisschen geschaut und die
+    # Qualität von deutschen Stemmern ist relativ bescheiden
+    
+    # iterate over all documents and tokenize each text
+    i = 0
+    for text in helper[column_name]:
+        # text = text.lower()
+        # remove special characters and do tokenization
+        text = np.array(tokenizer.tokenize(text))
+        #remove stopwords
+        text = [word for word in text if not word in cachedStopWords]
+        
+        helper.at[i,"pre_text"] = text
+        i += 1
+        
+    return helper
+
+def plot(meta_data, embeddings, nr_samples, sent_vec_gen_method, dim_red_method, color):
+
+    df = meta_data[:nr_samples][["classifications", "subjects"]]
+    # title gets cut off after "length" characters, otherwise hoverlabel is too long
+    length = 75
+    df["title"] = meta_data["title"][:nr_samples].apply(lambda x: x[:length] if len(x)>length else x)
+    df["x"] = embeddings[:,0]
+    df["y"] = embeddings[:,1]
+    df["z"] = embeddings[:,2]
+    df.fillna('NaN', inplace=True)
+
+    title = f"Visualization of: {sent_vec_gen_method} + {dim_red_method} + {nr_samples} Samples + Color:{color}"
+
+    fig = px.scatter_3d(df, 
+                        x='x', y='y', z='z', 
+                        color=color, 
+                        hover_name="title", # what to show when hovered over
+                        width=2500, height=1250, # adjust height and width
+                        title=title)
+
+    # make set size for legend and hover label
+    fig.update_layout(showlegend=True,
+                     legend = dict(
+                            font = dict(size = 10)
+                            ), 
+                    hoverlabel=dict(
+                            font_size=9,
+                            )
+                    )
+
+    # set marker size
+    fig.update_traces(marker_size = 3)
+
+    fig.show()
+
