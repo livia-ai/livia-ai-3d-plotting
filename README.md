1d59ccab3a5d562c71ff00fa73ff00f2c3a541a0
Rainer Simon <aboutgeo@gmail.com>
2022-04-12 10:24:10 +0200

    Minor tweaks

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-Python Script to create 3D plots with Plotly: main.py <br>
+Python Script to create 3D plots with Plotly: main.py
+
 -> sentence_embeddings_wien_museum.csv is on Google Drive in folder 04_Data
+
+## Some minor enhancements/questions
+
+- [ ] We don't really need the text pre-processing step for the vis, if I understand correctly
+- [ ] Can we sample N records? (Right now, we're picking the first N)
+- [ ] Can we add the record ID to the sentence embeddings file? (So we can use it as a standalone dataset)
+- [ ] It looks like the sentence embeddings correlate better with the classficiations rather than the subjects. I wonder why that's the case?
--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import pandas as pd
 import utility_functions.utility_functions as utils
 from sklearn.decomposition import PCA
 
+import nltk
+nltk.download('stopwords')
+
 # data loading and preprocessing
 # load wien museum data 
 wm_original = pd.read_csv("wien_museum.csv") # note  file location
-# take only interesting columns
+# take only interesting columns (id, title, artistsProducers, classifications, dates, districts, subjects)
 wm_filtered = wm_original[wm_original.columns[[0,3,4,5,6,7,8]]]
 # merge text data of all columns into one 
 wm_filtered = wm_filtered.assign(full_text = wm_filtered[wm_filtered.columns[1:]].apply(lambda x: ' '.join(x.dropna().astype(str)),axis=1))
@@ -19,7 +22,7 @@ sentence_embeddings = np.loadtxt('sentence_embeddings_wien_museum.csv', delimite
 print("Loading: Done")
 
 # create subset of sentence embeddings (62591 = all samples)
-nr_samples = 62591 
+nr_samples = 10000 
 sub_sample = sentence_embeddings[:nr_samples]
 
 # perform PCA: down project to 3 dimensional vector
