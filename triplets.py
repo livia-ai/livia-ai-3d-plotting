9c316fcee87482ffd96644fd85675fc52faf9579
Bernhard Franzl <b.franzl.uni@outlook.com>
2022-04-20 10:49:29 +0200

    added clustering.py + some examples

---
+++ triplets.py
@@ -0,0 +1,47 @@
+# imports
+import numpy as np
+import pandas as pd
+from utility_functions import utility_functions as utils
+import sklearn
+from sklearn.decomposition import PCA
+import sklearn.neighbors as neighbors
+
+# load wien museum data 
+wm_original = pd.read_csv("data/wien_museum.csv") # note  file location
+# take only interesting columns
+wm_filtered = wm_original[wm_original.columns[[0,3,4,5,6,7,8]]]
+# merge text data of all columns into one 
+wm_filtered = wm_filtered.assign(full_text = wm_filtered[wm_filtered.columns[1:]].apply(lambda x: ' '.join(x.dropna().astype(str)),axis=1))
+# load sentence embedding
+sentence_embeddings = np.loadtxt('data/sentence_embeddings_wien_museum.csv', delimiter=',')
+
+# standardize data
+stand_sentence_embeddings = (sentence_embeddings - np.mean(sentence_embeddings, axis=0)) / np.std(sentence_embeddings, axis=0)
+stand_pca = PCA(n_components=3)
+coordinates_3d = stand_pca.fit_transform(stand_sentence_embeddings)
+
+# create a dataframe that contains the needed meta data (mostly for plotting)
+triplet_dataframe = wm_filtered[["id","classifications", "subjects", "full_text"]]
+triplet_dataframe  = triplet_dataframe.assign(title = wm_filtered["title"][:].apply(lambda x: x[:75] if len(x)>75 else x))
+triplet_dataframe  = triplet_dataframe.assign(x = coordinates_3d[:,0])
+triplet_dataframe  = triplet_dataframe.assign(y = coordinates_3d[:,1])
+triplet_dataframe  = triplet_dataframe.assign(z = coordinates_3d[:,2])
+
+queries = None
+
+# sample n query samples randomly 
+n = 30
+rng = np.random.default_rng()
+query_ids = rng.integers(low=0, high=len(stand_sentence_embeddings), size=n)
+queries = stand_sentence_embeddings[query_ids]
+
+full_information, triplets = utils.create_triplets(queries, query_ids, stand_sentence_embeddings, "cosine", 15)
+
+# displays one randomly chosen triplet
+#utils.display_one_triplet(triplets, triplet_dataframe)
+
+# displays all triplets
+# utils.display_all_triplets(triplets, triplet_dataframe)
+
+# displays first sample with all k simialar and dissimilar samples
+utils.display_all_dis_similar(query_ids, full_information, triplet_dataframe)
--- utility_functions/utility_functions.py
+++ utility_functions/utility_functions.py
@@ -10,6 +10,9 @@ import plotly.express as px
 import altair as alt
 import altair_saver as asav
 
+import sklearn
+
+
 def preprocessing(text_data: pd.DataFrame, column_name: str) -> pd.DataFrame:
 
     helper = text_data.copy(deep = True)
@@ -151,3 +154,199 @@ def time_function(function, x):
     elapsed_time = end - start
 
     return output, elapsed_time
+
+def calc_distances(query, data, metric, k):
+    
+    if query.shape[0] == 1:
+        # claculate distance of query to all data points in data
+        dists = sklearn.metrics.pairwise_distances(query, data, metric=metric)[0]
+        # zip ids to distances for sorting
+        zipped_dists = np.array(list(zip(range(len(dists)),dists)))
+        # sort according to distances
+        sorted_dists = np.array(sorted(zipped_dists, key  = lambda x: x[1]))
+
+        min_k_ids = sorted_dists[1:k+1, 0]
+        min_k_dists = sorted_dists[1:k+1, 1]
+
+        max_k_ids = sorted_dists[-k:, 0]
+        max_k_dists = sorted_dists[-k:, 1]
+
+        return min_k_ids, min_k_dists, max_k_ids, max_k_dists
+    
+    else:
+        # claculate distance of query to all data points in data
+        dists_matrix = sklearn.metrics.pairwise_distances(query, data, metric=metric)
+        #print(dists_matrix.shape)
+        
+        results = list()
+        for dists in dists_matrix:
+            # zip ids to distances for sorting
+            zipped_dists = np.array(list(zip(range(len(dists)),dists)))
+            # sort according to distances
+            sorted_dists = np.array(sorted(zipped_dists, key  = lambda x: x[1]))
+
+            min_k_ids = sorted_dists[1:k+1, 0]
+            min_k_dists = sorted_dists[1:k+1, 1]
+            
+            max_k_ids = sorted_dists[-k:, 0]
+            max_k_dists = sorted_dists[-k:, 1]
+
+            results.append((min_k_ids, min_k_dists, max_k_ids, max_k_dists))
+
+        return results
+
+def create_triplets(query, query_ids, embeddings, metric, k):
+    
+    neighborhoods = calc_distances(query, embeddings, metric, k)
+    
+    rng = np.random.default_rng()
+    res = list()
+    triplets = list()
+    
+    i = 0
+    for min_k_ids, min_k_dists, max_k_ids, max_k_dists in neighborhoods:
+        min_id = rng.integers(k)
+        max_id = rng.integers(k)
+
+        res.append((int(min_k_ids[min_id]), min_k_dists[min_id],
+                    int(max_k_ids[max_id]), max_k_dists[max_id]))
+        
+        triplets.append([query_ids[i], int(min_k_ids[min_id]), int(max_k_ids[max_id])])
+        
+        i += 1
+        
+    return neighborhoods, triplets
+
+def display_one_triplet(triplets, dataframe):
+    
+    length = len(triplets)
+    
+    rng = np.random.default_rng()
+    idx = rng.integers(length)
+    
+    sample_id, similar_id, dissimilar_id = triplets[idx]
+    print("Triplet IDs:", triplets[idx])
+    
+    sample_data = dataframe.loc[sample_id]
+    similar_data = dataframe.loc[similar_id]
+    dissimilar_data = dataframe.loc[dissimilar_id]
+        
+    sample_text = sample_data["full_text"]
+    similar_text = similar_data["full_text"]
+    disimilar_text = dissimilar_data["full_text"]
+    
+    triplet_df = dataframe.loc[triplets[idx]]
+    triplet_df = triplet_df.assign(label = ["sample", "similar", "disimilar"])
+    
+    print("Sample:\n", sample_text)
+    print()
+    print("Similar:\n", similar_text)
+    print()
+    print("Dissimilar:\n", disimilar_text)
+    print()
+    
+    fig = px.scatter_3d(triplet_df, x='x', y='y', z='z', 
+                    color='label', hover_name="title", hover_data=["id", "classifications"],
+                    width=1100, height=800,# adjust height and width
+                    title="One triplet")
+
+    fig.update_layout(legend = dict(
+                            font = dict(size = 10)
+                            ), 
+
+                      hoverlabel=dict(
+                            font_size=9,
+                            )
+                    )
+
+    fig.update_traces(marker_size = 3)
+
+    fig.show()
+
+def display_all_triplets(triplets, dataframe):
+    
+    triplets_df = pd.DataFrame()
+    i = 0
+    for triplet in triplets:
+        
+        triplet_df = dataframe.loc[triplet]
+        triplet_df = triplet_df.assign(label = ["sample", "similar", "disimilar"])
+        triplet_df = triplet_df.assign(sample_id = str(i))
+        
+        triplets_df = pd.concat([triplets_df, triplet_df])
+        
+        i += 1
+            
+    triplets_df.reset_index(drop=True, inplace=True)
+    
+    
+    fig = px.scatter_3d(triplets_df, x='x', y='y', z='z', 
+                    color='sample_id', hover_name="title", hover_data=["id", "classifications", "label"],
+                    width=1100, height=800,# adjust height and width
+                    title="All triplets")
+
+    fig.update_layout(legend = dict(
+                            font = dict(size = 10)
+                            ), 
+
+                      hoverlabel=dict(
+                            font_size=9,
+                            )
+                    )
+
+    fig.update_traces(marker_size = 3)
+
+    fig.show()
+
+def display_all_dis_similar(query_ids, neighborhoods, dataframe):
+    
+
+    idx = query_ids[0]
+    neighborhood = neighborhoods[0]
+    
+    sim_ids = neighborhood[0]
+    dissim_ids = neighborhood[2]
+    
+    sample_df = dataframe.loc[idx].to_frame().transpose()
+    sample_df = sample_df.assign(label = ["sample"])
+    
+    sim_df = dataframe.loc[sim_ids]
+    sim_df = sim_df.assign(label = ["similar"]*len(sim_ids))
+    
+    dissim_df = dataframe.loc[dissim_ids]
+    dissim_df = dissim_df.assign(label = ["disimilar"]*len(dissim_ids))
+    
+    df = pd.concat([sample_df, sim_df, dissim_df])
+    df.reset_index(drop=True, inplace=True)
+    print(df)
+    
+    fig = px.scatter_3d(df, x='x', y='y', z='z', 
+                    color='label', hover_name="title", hover_data=["id", "classifications"],
+                    width=1100, height=800,# adjust height and width
+                    title="All similar and dissimlar samples for one sample")
+
+    fig.update_layout(legend = dict(
+                            font = dict(size = 10)
+                            ), 
+
+                      hoverlabel=dict(
+                            font_size=9,
+                            )
+                    )
+
+    fig.update_traces(marker_size = 3)
+
+    fig.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
