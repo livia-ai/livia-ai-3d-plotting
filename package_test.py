8f16b66e18795dcd19550d275e8d89c560e1d191
Bernhard Franzl <b.franzl.uni@outlook.com>
2022-05-10 13:30:39 +0200

    livia package update - added embedding generator

--- package_test.py
+++ package_test.py
@@ -1,16 +1,20 @@
-import livia.triplet as tr
 
-import numpy as np
-embedding_path = 'data/se_wm_100d.csv'
-# load sentence embedding 100d
-sentence_embeddings = np.loadtxt(embedding_path, delimiter=',', usecols=range(1,101))
-# load museum ids
-ids = np.loadtxt(embedding_path, delimiter=',', usecols=0, dtype=str)
+#######################################
+# triplets
 
-n = 10
+#import numpy as np
+#import livia.triplet as tr
 
-# create instance of class Tiplet
-triplet = tr.Triplet(sentence_embeddings, ids)
+#embedding_path = 'data/se_wm_100d.csv'
+## load sentence embedding 100d
+#sentence_embeddings = np.loadtxt(embedding_path, delimiter=',', usecols=range(1,101))
+## load museum ids
+#ids = np.loadtxt(embedding_path, delimiter=',', usecols=0, dtype=str)
+
+#n = 10
+
+## create instance of class Tiplet
+#triplet = tr.Triplet(sentence_embeddings, ids)
 
 ## generate triplets
 #triplet.generate_triplets(method="clustering",
@@ -24,3 +28,20 @@ triplet = tr.Triplet(sentence_embeddings, ids)
 
 ## compare performance: brute-force vs clustering algo
 #triplet.performance_comparison_plot(n_list=[1,10,100,300])
+#######################################
+
+
+#######################################
+# embedding
+
+import pandas as pd
+import livia.embedding as emb
+
+embedding = emb.Embedding()
+
+wm_data = pd.read_csv("data/belvedere.csv")
+emb_columns = ["Title", "Description"]
+id_column = "Identifier"
+embedding_dimensions = 100
+embedding.generate_embedding(wm_data, emb_columns, id_column, embedding_dimensions)
+
