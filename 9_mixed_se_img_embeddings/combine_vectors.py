35a91dff77752c56506b6d252a36fde2bda800ab
Rainer Simon <aboutgeo@gmail.com>
2022-10-06 15:06:41 +0200

    Minor tweaks

--- 9_mixed_se_img_embeddings/combine_vectors.py
+++ 9_mixed_se_img_embeddings/combine_vectors.py
@@ -18,9 +18,9 @@ with open(se_csv, 'r') as infile:
     se.append(row)
 
 # Load 128d image embeddings
-# ie_csv = '../../data/Belvedere/bel_image_embedding.csv'
-# ie_csv = '../../data/Wien_Museum/wm_image_embedding.csv'
-ie_csv = '../../data/MAK/mak_image_embedding_priref.csv'
+# ie_csv = '../../data/Belvedere/img2vec_Belvedere_128d.csv'
+# ie_csv = '../../data/Wien_Museum/img2vec_Wien_Museum_128d.csv'
+ie_csv = '../../data/MAK/img2vec_MAK_128d.csv'
 
 ie = {}
 
@@ -32,10 +32,10 @@ with open(ie_csv, 'r') as infile:
     vec = row[1:]
     ie[id] = vec
 
-print('Reducing sentence embeddings to 128d')
+print('Reducing sentence embeddings to 64d')
 
 # Run PCA on sentence embeddings, to reduce to 128d
-pca = PCA(n_components=128, svd_solver='full')
+pca = PCA(n_components=64, svd_solver='full')
 se_reduced = pca.fit_transform(np.array([ row[1:] for row in se ]))
 se_reduced = [ row.tolist() for row in se_reduced ]
 
@@ -43,13 +43,13 @@ print('Concatenating vectors')
 
 combined = []
 
-for idx, se_128 in enumerate(se_reduced):
+for idx, se_64 in enumerate(se_reduced):
   id = se[idx][0]
 
   if id in ie:
     ie_128 = ie[id]
 
-    c = [ id ] + ie_128 + se_128
+    c = [ id ] + se_64 + ie_128
 
     combined.append(c)
   else:
@@ -57,6 +57,6 @@ for idx, se_128 in enumerate(se_reduced):
 
 print('Writing results to file')
 
-with open('combined_embeddings_256d.csv', 'w') as outfile:  
+with open('combined_embeddings_192d.csv', 'w') as outfile:  
   writer = csv.writer(outfile)
   writer.writerows(combined)
